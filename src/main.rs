1454dcd97443387df35630482bf072711d73c546
Anton <akumaigorodski@gmail.com>
2025-03-09 18:50:44 +0200

    Refactor function to inline code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rustfft::num_traits::Zero;
 use rustfft::{num_complex::Complex, num_traits::Pow};
 use std::sync::Arc;
 use std::{f32::consts::PI, time::Instant};
@@ -14,7 +15,7 @@ type ComplexF32 = Complex<f32>;
 pub struct MOSSETracker {
     window_center: (f32, f32),
     window_size: (usize, usize),
-    han_window: Vec<f32>,
+    hanning_window: Vec<f32>,
     g: Vec<ComplexF32>,
     h: Vec<ComplexF32>,
     a: Vec<ComplexF32>,
@@ -23,59 +24,58 @@ pub struct MOSSETracker {
     fft: Arc<dyn Fft<f32>>,
 }
 
-#[inline]
-fn real2complex(real: f32) -> ComplexF32 {
-    Complex::new(real, 0f32)
-}
-
 impl MOSSETracker {
     pub fn new(w: usize, h: usize) -> Self {
-        let han_win = Self::hanning_window(w, h);
-        let gaussian = Self::gaussian_center_peak(w, h);
+        let window_center = (w as f32 / 2f32, h as f32 / 2f32);
+        let length = w * h;
 
-        let mut planner = FftPlanner::new(/**/);
-        let fft = planner.plan_fft_forward(w * h);
-        let inv_fft = planner.plan_fft_inverse(w * h);
-        fft.process(&mut gaussian);
-
-    }
-
-    fn hanning_window(w: usize, h: usize) -> Vec<f32> {
-        let mut window = vec![0f32; w * h];
-
-        window.par_chunks_mut(w).enumerate(/**/).for_each(|yrow| {
+        let mut hanning_window = vec![0f32; length];
+        let mut gaussian = vec![0f32; length];
+        
+        // TODO: precompute for max possible w * h, then take slices
+        hanning_window.par_chunks_mut(w).enumerate(/**/).for_each(|yrow| {
             let wy = (PI * yrow.0 as f32 / (h - 1) as f32).sin(/**/);
 
             for x in 0..w {
-                let wx = (PI * x as f32 / (w - 1) as f32).sin(/**/);
-                yrow.1[x] = wx * wy;
+                let wx = PI * x as f32 / (w - 1) as f32;
+                yrow.1[x] = wx.sin(/**/) * wy;
             }
         });
 
-        window
-    }
-
-    fn gaussian_center_peak(w: usize, h: usize) -> Vec<ComplexF32> {
-        let (cx, cy) = (w as f32 / 2.0, h as f32 / 2.0);
-        let mut gaussian = vec![0f32; w * h];
-        
         // Calculate gaussian distribution with a peak at center
         gaussian.par_chunks_mut(w).enumerate(/**/).for_each(|yrow| {
-            let dy = (yrow.0 as f32 - cy).powi(2);
+            let dy = (yrow.0 as f32 - window_center.1).powi(2);
 
             for x in 0..w {
-                let dx = (x as f32 - cx).powi(2);
+                let dx = (x as f32 - window_center.0).powi(2);
                 yrow.1[x] = (-(dx + dy) / 2f32).exp(/**/);
             }
         });
 
-        // Normalize and turn into Complex for subsequent FFT
+        
         let max_value = gaussian.iter(/**/).cloned(/**/).fold(0./0., f32::max);
-        let res = gaussian.into_par_iter(/**/).map(|v| Complex::new(v / max_value, 0f32));
-        res.collect(/**/)
+        let complex2real = |current_value| Complex::new(current_value / max_value, 0f32);
+        let mut complex: Vec<ComplexF32> = gaussian.par_iter(/**/).map(complex2real).collect(/**/);
+
+        let mut planner = FftPlanner::new(/**/);
+        let fft = planner.plan_fft_forward(length);
+        let inv_fft = planner.plan_fft_inverse(length);
+        fft.process(&mut complex);
+
+        Self {
+            window_center,
+            window_size: (w, h),
+            hanning_window,
+            g: complex,
+            h: vec![Complex::zero(/**/); length],
+            a: vec![Complex::zero(/**/); length],
+            b: vec![Complex::zero(/**/); length],
+            inv_fft,
+            fft,
+        }
     }
 }
 
 fn main() {
-    
+    MOSSETracker::new(500, 500);
 }
